1adc989d66f9ea7bd670476a209e64032d4e3ff0
tasin <tasinishmam@gmail.com>
2020-03-21 01:14:50 +0600

    Added mongoose schema for quiz users

---
+++ database/models/users_quiz.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+const validator = require('validator');
+
+
+let users_quiz_schema =  new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+users_quiz_schema.plugins(beautifyUnique);
+
+const UsersQuiz = mongoose.model("UsersQuiz", users_quiz_schema);
+
+module.exports = {UsersQuiz};
