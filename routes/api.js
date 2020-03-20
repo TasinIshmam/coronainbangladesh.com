114d0e5ed2c63a3addb70f29aa95891fe6079ead
tasin <tasinishmam@gmail.com>
2020-03-20 15:49:06 +0600

    Added GET /api/myths route

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let api_mythbuster_controller = require('../controllers/api-mythbuster-controller');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/myths', api_mythbuster_controller.handle_get_myths );
+
+
 
 module.exports = router;
