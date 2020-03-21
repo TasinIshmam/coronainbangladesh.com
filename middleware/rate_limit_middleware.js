290ebb785b4e2a2ebc2769c85f56633027478f10
tasin <tasinishmam@gmail.com>
2020-03-21 16:43:58 +0600

    Temporarily lifted rate limiter because of bug

--- middleware/rate_limit_middleware.js
+++ middleware/rate_limit_middleware.js
@@ -1,26 +1,63 @@
-const { RateLimiterMongo } = require('rate-limiter-flexible');
-const {mongoose} = require('../database/mongoose');
+
+
+//const rateLimit = require("express-rate-limit");
 
 
 
-const max_requests_every_second = 30;
+// const limiter = rateLimit({
+//     windowMs: 1 * 60 * 1000, // 15 minutes
+//     max: 100 // limit each IP to 100 requests per windowMs
+// });
 
-const request_per_second_rate_limit = new RateLimiterMongo({
-    storeClient: mongoose.connection,
-    keyPrefix: 'too_many_requests_ip_per_second',
-    points: max_requests_every_second,
-    duration: 1,
-    blockDuration: 120, // Block for one minute if requests exceeded.
-});
+//  apply to all requests
 
-const rate_limit_per_second_middleware = (req, res, next) => {
-    request_per_second_rate_limit.consume(req.ip)
-        .then(() => {
-            next();
-        })
-        .catch(() => {
-            res.status(429).send('Too Many Requests from this ip');
-        });
-};
+//module.exports = {rate_limit_middleware}
 
-module.exports = {rate_limit_per_second_middleware};
+
+
+
+
+
+
+
+
+
+
+
+
+// const { RateLimiterMongo } = require('rate-limiter-flexible');
+// const {mongoose} = require('../database/mongoose');
+//
+//
+//
+// const max_requests_every_second = 30;
+//
+// const request_per_second_rate_limit = new RateLimiterMongo({
+//     storeClient: mongoose.connection,
+//     keyPrefix: 'too_many_requests_ip_per_second',
+//     points: max_requests_every_second,
+//     duration: 1,
+//     blockDuration: 120, // Block for one minute if requests exceeded.
+// });
+//
+// const rate_limit_per_second_middleware = (req, res, next) => {
+//
+//     let ipconsumed = req.ip;
+//     console.debug(req.ip);
+//     console.debug(typeof req.ip);
+//     console.debug("HELLO WORLD");
+//
+//     if(!ipconsumed ) {
+//         ipconsumed = "::1";
+//         console.debug("::1")
+//     }
+//     request_per_second_rate_limit.consume(ipconsumed)
+//         .then(() => {
+//             next();
+//         })
+//         .catch(() => {
+//             res.status(429).send('Too Many Requests from this ip');
+//         });
+// };
+//
+// module.exports = {rate_limit_per_second_middleware};
