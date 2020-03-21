cc84f4c555777ccd042c7ca9985d0193cf21a75b
KAI_Monmoy <aftarulislam@gmail.com>
2020-03-21 06:47:22 +0600

    :sparkles: Integrated WhatIsCorona

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const live_news_interface = require('../database/interface/live_news_interface')
 const featured_news_interface = require('../database/interface/featured_news_interface');
 const myth_interface = require('../database/interface/myth_interface');
 
-/* GET home page. */
+/* GET bn home page. */
 router.get('/', async function(req, res, next) {
 	const stats = await stat_interface.get_statistics_bangladesh();
 	const featured_news = await featured_news_interface.getNewsBetweenDatesWithCount();
@@ -20,9 +20,35 @@ router.get('/', async function(req, res, next) {
 	});
 });
 
-// GET what is corona page.
-router.get('/corona', function(req, res, next) {
-	res.render('coronavirus');
+/* GET en home page. */
+router.get('/en', async function(req, res, next) {
+	const stats = await stat_interface.get_statistics_bangladesh();
+	const featured_news = await featured_news_interface.getNewsBetweenDatesWithCount();
+	const live_news = await live_news_interface.get_all_live_news();
+
+	res.render('index_en', {
+		stats: stats,
+		featured_news: featured_news,
+		live_news: live_news
+	});
+});
+
+/* GET bn home page. */
+router.get('/corona', async function(req, res, next) {
+	const stats = await stat_interface.get_statistics_world();
+
+	res.render('coronavirus', {
+		stats: stats
+	});
+});
+
+/* GET en home page. */
+router.get('/en/corona', async function(req, res, next) {
+	const stats = await stat_interface.get_statistics_world();
+
+	res.render('coronavirus_en', {
+		stats: stats
+	});
 });
 
 module.exports = router;
