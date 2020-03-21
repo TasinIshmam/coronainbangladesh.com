cde8d2717e5c956505349f160629b7b1246c6212
tasin <tasinishmam@gmail.com>
2020-03-21 17:02:53 +0600

    Added default count parameter to live_news_interface

--- database/interface/live_news_interface.js
+++ database/interface/live_news_interface.js
@@ -1,6 +1,6 @@
 const { LiveNews } = require('../models/live_news');
 
-async function get_all_live_news(count) {
+async function get_all_live_news(count = 50) {
 	try {
 		let result = LiveNews.find().select('-_id -__v').sort({ id: -1 }).limit(count);
 		return result;
