aefc0d809498411e4e8089dc36d778bcee1efc7b
tasin <tasinishmam@gmail.com>
2020-03-22 17:45:38 +0600

    Added insert many to live_news_interface

--- database/interface/live_news_interface.js
+++ database/interface/live_news_interface.js
@@ -5,8 +5,24 @@ async function get_all_live_news(count = 50) {
 		let result = LiveNews.find().select('-_id -__v').sort({ id: -1 }).limit(count);
 		return result;
 	} catch (e) {
+		console.error(e)
 		return {};
 	}
 }
 
-module.exports = { get_all_live_news };
+async function insert_live_news_many (live_news_array) {
+
+
+	try {
+		const res = await LiveNews.insertMany(live_news_array, {upsert: true, setDefaultOnInsert: true});
+		return live_news_array.length === res.length;
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
+
+
+
+}
+
+module.exports = { get_all_live_news, insert_live_news_many };
