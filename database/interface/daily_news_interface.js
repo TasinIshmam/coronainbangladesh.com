0ebab17689557344ce299cad5a17dcc1fa2ebde7
tasin <tasinishmam@gmail.com>
2020-03-23 20:53:30 +0600

    Added daily_news_interface. Currently sends static dummy data

---
+++ database/interface/daily_news_interface.js
@@ -0,0 +1,46 @@
+const moment = require('moment');
+
+
+
+
+
+async function get_BANGLA_daily_news_BD_with_date(targetDate) {
+
+    let dummy_news_bd_23_march = [
+
+    ];
+    return dummy_news_bd_23_march;
+}
+
+
+async function get_ENGLISH_daily_news_BD_with_date(targetDate) {
+
+    let dummy_news_bd_23_march = [
+
+    ];
+    return dummy_news_bd_23_march;
+}
+
+
+
+
+
+async function get_BANGLA_daily_news_GLOBAL_with_date(targetDate) {
+
+    let dummy_news_bd_23_march = [
+
+    ];
+    return dummy_news_bd_23_march;
+}
+
+
+async function get_ENGLISH_daily_news_GLOBAL_with_date(targetDate) {
+
+    let dummy_news_bd_23_march = [
+
+    ];
+    return dummy_news_bd_23_march;
+}
+
+
+module.exports = {get_BANGLA_daily_news_BD_with_date, get_ENGLISH_daily_news_BD_with_date, get_BANGLA_daily_news_GLOBAL_with_date, get_ENGLISH_daily_news_GLOBAL_with_date};
