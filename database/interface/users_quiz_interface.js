a2a6970678bb04d22cbe60dc1f07575b48b3a0b5
tasin <tasinishmam@gmail.com>
2020-03-26 14:47:56 +0600

    Refactored database_interfaces
    Improved logging
    Added better error/null handling

--- database/interface/users_quiz_interface.js
+++ database/interface/users_quiz_interface.js
@@ -1,14 +1,21 @@
 const {UsersQuiz} = require('../models/users_quiz');
 
-
+/**
+ * Adds a new user to the quizusers collection. Also used for daily_news newsletter subscribers
+ * @param user
+ * @returns {Promise<boolean>} - Weather insert operation was successful.
+ */
 
 async function add_new_user(user) {
 
     try {
         let ret = await UsersQuiz.create(user);
+        if (ret === undefined || ret === null) return false;
         return true;
     }   catch (e) {
-        console.error(e);
+         console.error("Error in add_new_user");
+         console.error(e);
+
         return false;
     }
 
