76117f79e898aaf0c9eafd9aa87a6d323e74c72a
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2020-04-03 05:34:05 +0600

    added get all district method to map data repository

--- data_repository/map_data_repository.js
+++ data_repository/map_data_repository.js
@@ -1,19 +1,20 @@
 let cache = require('../cache/cache_interface');
 let division_data_interface = require('../database/interface/division_data_interface');
+let district_data_interface = require('../database/interface/district_data_interface');
 
 
 const EXPIRATION_TIME = 900;
 
 
 /**
- * Get all method
+ * Get all division entries method
  * @returns {Promise<{DivisionData}[]|[]>}
  */
 async function get_division_all() {
     let key = "division_data_all";
     let response = cache.get_cached_data(key);
 
-    if(response.status) {
+    if (response.status) {
         return response.data;
     } else {
         let db_result = await division_data_interface.get_division_all();
@@ -23,4 +24,22 @@ async function get_division_all() {
 }
 
 
-module.exports = {get_division_all}
+/**
+ * Get all district entries method
+ * @returns {Promise<{DistrictData}[]|[]>}
+ */
+async function get_district_all() {
+    let key = "district_data_all";
+    let response = cache.get_cached_data(key);
+
+    if (response.status) {
+        return response.data;
+    } else {
+        let db_result = await district_data_interface.get_district_all();
+        cache.set_cache_with_exp(key, db_result, EXPIRATION_TIME);
+        return db_result;
+    }
+}
+
+
+module.exports = {get_division_all, get_district_all};
