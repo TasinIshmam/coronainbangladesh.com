26a516b8ac7cac33618505a0f5305e536c5ad355
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2020-04-03 04:52:00 +0600

    added district data schema in models

--- database/models/district_data.js
+++ database/models/district_data.js
@@ -1,50 +1,45 @@
 const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 
-
-const division_data_schema = new mongoose.Schema({
-
+const district_data_schema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-
     id: {
         type: Number,
         required: true
     },
-
-    quarantine_ongoing: {
+    division: {
+        type: String,
+        required: true
+    },
+    total_quarantined: {
         type: Number,
         required: true
     },
-
-    quarantine_complete: {
+    passengers_screened: {
         type: Number,
         required: true
     },
-
-    isolation_beds: {
-        type: Number,
-        required: true
-    },
-
     last_update: {
         type: Date,
         required: false,
         default: Date.now
     }
+
 });
 
 
-division_data_schema.index( {"id" : 1} , {unique: true});
-division_data_schema.index( {"name" : 1} , {unique: true});
 
-division_data_schema.plugin(beautifyUnique);
+district_data_schema.index( {"id" : 1} , {unique: true});
+district_data_schema.index( {"name" : 1} , {unique: true});
+
+district_data_schema.plugin(beautifyUnique);
 
 
-let DivisionData = mongoose.model("DivisionData", division_data_schema);
+let DistrictData = mongoose.model("DivisionData", district_data_schema);
 
-DivisionData.createIndexes();  //idempotent operation.
+DistrictData.createIndexes();  //idempotent operation.
 
-module.exports = {DivisionData};
+module.exports = {DistrictData};
