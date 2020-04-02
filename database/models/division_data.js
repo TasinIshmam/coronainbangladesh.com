28d5134dbb04f62661d208926ea7e97335ebf05d
tasin <tasinishmam@gmail.com>
2020-04-02 08:35:23 +0600

    :construction: Division Data model created

---
+++ database/models/division_data.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+
+const division_data_schema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    id: {
+        type: Number,
+        required: true
+    },
+
+    quarantine_ongoing: {
+        type: Number,
+        required: true
+    },
+
+    quarantine_complete: {
+        type: Number,
+        required: true
+    },
+
+    isolation_beds: {
+        type: Number,
+        required: true
+    }
+});
+
+
+division_data_schema.index( {"id" : 1} , {unique: true});
+division_data_schema.index( {"name" : 1} , {unique: true});
+
+
+
+let DivisionData = mongoose.model("DivisionData", division_data_schema);
+
+DivisionData.createIndexes();  //idempotent operation.
+
+module.exports = {DivisionData};
